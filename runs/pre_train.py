555ca5e35cf444dabde9fcd912f737e5285e8619
AaronNZH <45916418+AaronNZH@users.noreply.github.com>
2023-07-10 16:42:08 +0800

    Update pre_train.py

--- runs/pre_train.py
+++ runs/pre_train.py
@@ -22,26 +22,27 @@ from utils.checkpoint_util import save_pretrain_checkpoint
 class DGCNNSeg(nn.Module):
     def __init__(self, args, num_classes):
         super(DGCNNSeg, self).__init__()
-        self.encoder = DGCNN(args.edgeconv_widths, args.dgcnn_mlp_widths, args.pc_in_dim, k=args.dgcnn_k, return_edgeconvs=True)
+        self.encoder = DGCNN(args.edgeconv_widths, args.dgcnn_mlp_widths, args.pc_in_dim, k=args.dgcnn_k,
+                             return_edgeconvs=True)
         in_dim = args.dgcnn_mlp_widths[-1]
         for edgeconv_width in args.edgeconv_widths:
             in_dim += edgeconv_width[-1]
         self.segmenter = nn.Sequential(
-                            nn.Conv1d(in_dim, 256, 1, bias=False),
-                            nn.BatchNorm1d(256),
-                            nn.LeakyReLU(0.2),
-                            nn.Conv1d(256, 128, 1),
-                            nn.BatchNorm1d(128),
-                            nn.LeakyReLU(0.2),
-                            nn.Dropout(0.3),
-                            nn.Conv1d(128, num_classes, 1)
-                         )
+            nn.Conv1d(in_dim, 256, 1, bias=False),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2),
+            nn.Conv1d(256, 128, 1),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(0.3),
+            nn.Conv1d(128, num_classes, 1)
+        )
 
     def forward(self, pc):
         num_points = pc.shape[2]
         edgeconv_feats, point_feat = self.encoder(pc)
         global_feat = point_feat.max(dim=-1, keepdim=True)[0]
-        edgeconv_feats.append(global_feat.expand(-1,-1,num_points))
+        edgeconv_feats.append(global_feat.expand(-1, -1, num_points))
         pc_feat = torch.cat(edgeconv_feats, dim=1)
 
         logits = self.segmenter(pc_feat)
@@ -69,12 +70,12 @@ def metric_evaluate(predicted_label, gt_label, NUM_CLASS):
             positive_classes[pred_l] += 1
             true_positive_classes[gt_l] += int(gt_l == pred_l)
 
-    oa = sum(true_positive_classes)/float(sum(positive_classes))
+    oa = sum(true_positive_classes) / float(sum(positive_classes))
     print('Overall accuracy: {0}'.format(oa))
     iou_list = []
 
     for i in range(NUM_CLASS):
-        iou_class = true_positive_classes[i] / float(gt_classes[i]+positive_classes[i]-true_positive_classes[i])
+        iou_class = true_positive_classes[i] / float(gt_classes[i] + positive_classes[i] - true_positive_classes[i])
         print('Class_%d: iou_class is %f' % (i, iou_class))
         iou_list.append(iou_class)
 
@@ -115,7 +116,7 @@ def pretrain(args):
                                       pc_augm=args.pc_augm, pc_augm_config=PC_AUGMENT_CONFIG)
 
     logger.cprint('=== Pre-train Dataset (classes: {0}) | Train: {1} blocks | Valid: {2} blocks ==='.format(
-                                                     CLASSES, len(TRAIN_DATASET), len(VALID_DATASET)))
+        CLASSES, len(TRAIN_DATASET), len(VALID_DATASET)))
 
     TRAIN_LOADER = DataLoader(TRAIN_DATASET, batch_size=args.batch_size, num_workers=args.n_workers, shuffle=True,
                               drop_last=True)
@@ -132,8 +133,8 @@ def pretrain(args):
         model.cuda()
 
     optimizer = optim.Adam([{'params': model.encoder.parameters(), 'lr': args.pretrain_lr}, \
-                           {'params': model.segmenter.parameters(), 'lr': args.pretrain_lr}], \
-                            weight_decay=args.pretrain_weight_decay)
+                            {'params': model.segmenter.parameters(), 'lr': args.pretrain_lr}], \
+                           weight_decay=args.pretrain_weight_decay)
     # Set learning rate scheduler
     lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.pretrain_step_size, gamma=args.pretrain_gamma)
 
@@ -161,7 +162,7 @@ def pretrain(args):
 
         lr_scheduler.step()
 
-        if (epoch+1) % args.eval_interval == 0:
+        if (epoch + 1) % args.eval_interval == 0:
             pred_total = []
             gt_total = []
             model.eval()
